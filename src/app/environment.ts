b22d87fab37935b35f542e23a05ea13bccecfae8
Mishka1108 <mr.mishka3003@gmail.com>
2025-10-18 15:40:46 -0700

    envi

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:10000/api',
-    socketUrl: 'http://localhost:10000'
+    apiUrl: 'https://fullproject-xtbq.onrender.com/api',
+    socketUrl: 'https://fullproject-xtbq.onrender.com'
   };
